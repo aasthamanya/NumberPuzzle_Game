34da5091961a012a083159cab796a9303eda295e
aasthamanya <54213825+aasthamanya@users.noreply.github.com>
2021-07-09 13:17:52 +0530

    Changes in 3 functions.

--- NumberPuzzleQuestion/src/NumberPuzzleControl.java
+++ NumberPuzzleQuestion/src/NumberPuzzleControl.java
@@ -41,4 +41,57 @@ abstract class NumberPuzzleControl {
 		}
 		return buttonIds;
 	} 
+	public int[] getRandomNumbersForGrid()
+	{
+		int arr[] = new int[15];
+		int x = getRandomNumber();
+		x = (x%15)+1;
+		int j=0;
+		boolean check = false;
+		while(j<15)
+		{
+			check = false;
+			for(int i=0;i<arr.length;i++)
+			{
+				if(arr[i]==x)
+				{
+					check = true;
+				}
+			}
+			if(check==false)
+			{
+				arr[j]=x;
+				j++;
+			}
+			else
+			{
+			x = getRandomNumber();
+			x = (x%15)+1;
+			}
+		}
+		return arr;
+	}
+	public int handleButtonClicked(NumberPuzzleGame game)
+	{
+		int emptyCellId = game.getEmptyCellId();
+		Button buttonClicked = game.getButtonClicked();
+		Button[] buttons = game.getButtons();
+		swapButton(Button[emptyCellId], buttonClicked);
+		return emptyCellId;
+	}
+	public boolean checkForWinner(Button buttons[])
+	{
+		boolean winner = true;
+		Button buttons = game.getButtons();
+		int arr[] = getIntegerArrayOfButtonIds(buttons);
+		for(int i=1;i<arr.length();i++)
+		{
+			if(arr[i-1]>arr[i])
+			{
+				winner = false;
+				break;
+			}
+		}
+		return winner;
+	}
 }
